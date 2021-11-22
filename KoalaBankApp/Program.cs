1000c20db508458ae2d8c08c17f94fcf8b03793b
Ejpatolitej <niklassonjack@gmail.com>
2021-11-22 15:28:59 +0100

    Test Jack

--- KoalaBankApp/Program.cs
+++ KoalaBankApp/Program.cs
@@ -8,6 +8,7 @@ namespace KoalaBankApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hej från mac!");
+            Console.WriteLine("JACK WAS HERE");
         }
     }
 }
