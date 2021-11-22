80d3499d42f8950c10c95bc1badd0d723e9a1eb2
Elias Lövdinger <elias.lovdinger@edu.varberg.se>
2021-11-22 15:28:34 +0100

    test Elias

--- KoalaBankApp/Program.cs
+++ KoalaBankApp/Program.cs
@@ -7,6 +7,14 @@ namespace KoalaBankApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+            Console.WriteLine("Test ifrån Elias");
         }
     }
 }
