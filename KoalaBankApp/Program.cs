12b730bf6d6947fecad6e223ad00b1cdb0ff58a9
ludwigOleby <oleby.ludwig@gmail.com>
2021-11-22 15:44:32 +0100

    Update Program.cs
    test ludwig

--- KoalaBankApp/Program.cs
+++ KoalaBankApp/Program.cs
@@ -9,6 +9,18 @@ namespace KoalaBankApp
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hej från mac!");
             Console.WriteLine("Hej ifrån Elias");
+
+            bool tired = true;
+
+            if (tired = true)
+            {
+                Console.WriteLine("more coffee");
+
+            }
+            else
+            {
+                Console.WriteLine("no more coffee");
+            }
         }
     }
 }
