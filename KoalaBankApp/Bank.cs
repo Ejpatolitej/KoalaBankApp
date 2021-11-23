9fb50947be746821c1b4000d67da5ee9c542e3d1
LucasNarfgren <narfgren@hotmail.com>
2021-11-23 14:05:11 +0100

    Working on classes in Account

--- KoalaBankApp/Bank.cs
+++ KoalaBankApp/Bank.cs
@@ -6,21 +6,36 @@ namespace KoalaBankApp
 {
     class Bank
     {
-        List<Accounts> Accounts = new List<Accounts>();
-        BankAccount[] Useraccount = new BankAccount[5];
+
 
         public void Run()
         {
+            List<Account> Accounts = new List<Account>();
 
-            LogIn(Accounts);
+            List<BankAccount> BAList1 = new List<BankAccount>();
+            BankAccount BAccount1 = new BankAccount();
+            Account Account1 = new Account("Lukkelele", "hejhej123", "Lucas", "Narfgren", "narfgren@hotmail.com", BAList1);
+            Account1.Useraccount.Add(BAccount1);
+            Accounts.Add(Account1);
+            
 
 
-        }
+            //foreach (var item in Accounts)
+            //{
+            //    Console.WriteLine(item._UserName);
+            //    Console.WriteLine(item.Firstname);
+            //    Console.WriteLine(item.Lastname);
+            //    Console.WriteLine(item.Password);
+            //    Console.WriteLine(item.Useraccount);
+            //    foreach (var i in B1)
+            //    {
+            //        Console.WriteLine(i.AccountName); 
+            //        Console.WriteLine(i.Balance);
+            //    }
+            //}
 
-        public bool LogIn(List<Accounts> Accounts)
-        {
 
-            return true;
+
         }
     }
 }
